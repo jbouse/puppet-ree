70fbb36098e8f9d976588103930eea6e8c06d1c3
Jeremy T. Bouse <jtbouse@gmail.com>
2011-11-12 21:57:22 -0500

    Clean-up from testing

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,19 @@
 class ree (
-    $version  = $ree::params::version,
-    $prefix   = $ree::params::prefix,
-    $packages = $ree::params::depends
-) inherits $ree::params {
+    $version   = $ree::params::version,
+    $prefix    = $ree::params::prefix,
+    $packages  = $ree::params::depends,
+    $sourceuri = $ree::params::source
+) inherits ree::params {
 
-    package { $packages: ensure => present }
+    package { 
+        $packages: 
+            ensure => present,
+            before => Exec["install Ruby Enterprise Edition"],
+    }
 
     exec {
         "install Ruby Enterprise Edition":
-            command => "curl -sS ${source}-${version}.tar.gz |tar -xzf - -C /tmp --no-same-owner && /tmp/ruby-enterprise-${version}/install --auto ${prefix} && rm -rf /tmp/ruby-enterprise-${version}",
+            command => "curl -sS ${sourceuri}-${version}.tar.gz |tar -xzf - -C /tmp --no-same-owner && /tmp/ruby-enterprise-${version}/install --auto ${prefix} && rm -rf /tmp/ruby-enterprise-${version}",
             creates => "${prefix}/bin/ree-version",
     }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,9 @@
 class ree::params {
-    $version = '1.8.7-2011.03'
-    $prefix  = '/opt/local'
-
-    $packages = $operatingsystem ? {
+    $version   = '1.8.7-2011.03'
+    $prefix    = '/opt/local'
+    $sourceuri = 'http://rubyenterpriseedition.googlecode.com/files/ruby-enterprise'
+    $packages  = $operatingsystem ? {
         /(?i-mx:ubuntu|debian)/        => ['libreadline5-dev', 'build-essential', 'libmysqlclient-dev'],
-        /(^i-mx:centos|fedora|redhat)/ => [''],
+        # /(^i-mx:centos|fedora|redhat)/ => [''],
     }
 }
